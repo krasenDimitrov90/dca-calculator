0a81a11b254b276901f1090e596c1979bdec313c
krasenDimitrov <krasendimitrov90@gmail.com>
2024-02-26 07:42:07 +0200

    [FE][FIX] portfolio table header not visible when scrolling

--- front-end/src/components/PortfolioChart/PortfolioChart.js
+++ front-end/src/components/PortfolioChart/PortfolioChart.js
@@ -4,49 +4,51 @@ import './PortfolioChart.styles.css';
 const Row = () => {
     return (
         <tr>
-            <td data-label="Account">Visa - 3412</td>
-            <td data-label="Due Date">04/01/2016</td>
-            <td data-label="Amount">$1,190</td>
-            <td data-label="Period">03/01/2016 - 03/31/2016</td>
+            <td data-label="Date">04/01/2016</td>
+            <td data-label="Btc price">30,234</td>
+            <td data-label="Invested">$1,190</td>
+            <td data-label="Btc purchesed">0.003</td>
+            <td data-label="Balance">$145</td>
         </tr>
     );
 };
 
 export const PortfolioChart = React.memo(() => {
     return (
-        // <div className='portfolio-chart-container'>
-        <table className='portfolio-table'>
-            <thead>
-                <tr>
-                    <th scope="col">Account</th>
-                    <th scope="col">Due Date</th>
-                    <th scope="col">Amount</th>
-                    <th scope="col">Period</th>
-                </tr>
-            </thead>
-            <tbody>
-                <Row />
-                <Row />
-                <Row />
-                <Row />
-                <Row />
-                <Row />
-                <Row />
-                <Row />
-                <Row />
-                <Row />
-                <Row />
-                <Row />
-                <Row />
-                <Row />
-                <Row />
-                <Row />
-                <Row />
-                <Row />
-                <Row />
-                <Row />
-            </tbody>
-        </table>
-        // </div>
+        <div className='portfolio-table-container'>
+            <table className='portfolio-table'>
+                <thead className='custom-gradient-secondary'>
+                    <tr>
+                        <th scope="col">Date</th>
+                        <th scope="col">Btc price</th>
+                        <th scope="col">Invested</th>
+                        <th scope="col">Btc purchesed</th>
+                        <th scope="col">Balance</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <Row />
+                    <Row />
+                    <Row />
+                    <Row />
+                    <Row />
+                    <Row />
+                    <Row />
+                    <Row />
+                    <Row />
+                    <Row />
+                    <Row />
+                    <Row />
+                    <Row />
+                    <Row />
+                    <Row />
+                    <Row />
+                    <Row />
+                    <Row />
+                    <Row />
+                    <Row />
+                </tbody>
+            </table>
+        </div>
     );
 });
