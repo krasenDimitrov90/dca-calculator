2366bcde556fbacde218f7411529c1ddf2c96b1f
krasenDimitrov <krasendimitrov90@gmail.com>
2024-03-17 10:48:39 +0200

    [FE] making an svg for the app

--- front-end/src/components/Navigation/Navigation.js
+++ front-end/src/components/Navigation/Navigation.js
@@ -30,7 +30,6 @@ export const Navigation = ({ currentBTCPrice }) => {
   useEffect(() => {
     if (price !== null && lastPriceRef.current !== null) {
       let lastPrice = lastPriceRef.current
-      console.log({ price, lastPrice }, price > lastPrice);
       setPriceWentUp(() => price > lastPrice);
       lastPriceRef.current = price;
     }
@@ -71,11 +70,13 @@ export const Navigation = ({ currentBTCPrice }) => {
 
   return (
     <div className='flex flex-col desktop:flex-row justify-between text-app-text-primary py-s'>
-      <div className='flex items-end py-app-xs'>
-        <div className='flex items-end h-full gap-[6px] py-[3px] mr-app-base'>
-          <span className='flex bg-app-blue-primary h-[25%] w-[3px] rounded'></span>
-          <span className='flex bg-app-blue-primary h-[60%] w-[3px] rounded'></span>
-          <span className='flex bg-app-blue-primary h-[96%] w-[3px] rounded'></span>
+      <div className='flex items-center py-app-xs'>
+        <div className='flex items-center h-full'>
+          <svg width="30" height="30" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M19 21C19 19.3431 20.3431 18 22 18H26C27.6569 18 29 19.3431 29 21V39C29 40.6569 27.6569 42 26 42H22C20.3431 42 19 40.6569 19 39V21Z" fill="#737CD9"></path>
+            <path d="M6 33C6 31.3431 7.34315 30 9 30H13C14.6569 30 16 31.3431 16 33V39C16 40.6569 14.6569 42 13 42H9C7.34315 42 6 40.6569 6 39V33Z" fill="#737CD9"></path>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M32 9C32 7.34315 33.3431 6 35 6H39C40.6569 6 42 7.34315 42 9V39C42 40.6569 40.6569 42 39 42H35C33.3431 42 32 40.6569 32 39V9ZM35 8C34.4477 8 34 8.44772 34 9V39C34 39.5523 34.4477 40 35 40H39C39.5523 40 40 39.5523 40 39V9C40 8.44772 39.5523 8 39 8H35Z" fill="#737CD9"></path>
+          </svg>
         </div>
         <h3 className='text-app-xl'>Dollar Cost Average</h3>
       </div>
@@ -83,7 +84,6 @@ export const Navigation = ({ currentBTCPrice }) => {
         fiatCurrency={currentFiatCurrency}
         priceWentUp={priceWentUp}
         assetPrice={currentFiatCurrency === 'BGN' ? Number(price) * 1.96 : Number(price)}
-      // assetPrice={currentBTCPrice[currentFiatCurrency.toLowerCase()]}
       />
       <Select
         variant={Select.variants.LABEL_LEFT}
